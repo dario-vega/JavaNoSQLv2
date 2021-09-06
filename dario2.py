ca6573340f288c00070e2254a8dd33431d64eea0
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-09-06 20:45:27 +0200

    Update dario2.py

--- dario2.py
+++ dario2.py
@@ -11,6 +11,7 @@ from borneo.iam import SignatureProvider
 
 signer = oci.auth.signers.InstancePrincipalsDelegationTokenSigner (delegation_token=os.getenv('OCI_obo_token'))
 NoSQLprovider = SignatureProvider(provider=signer)
+#NoSQLprovider = SignatureProvider(provider=signer, region=os.getenv('OCI_REGION'))
 NoSQLconfig = NoSQLHandleConfig(os.getenv('OCI_REGION'),NoSQLprovider).set_logger(None).set_default_compartment(os.getenv('NOSQL_COMP_ID'))
 #NoSQLconfig = NoSQLHandleConfig('us-ashburn-1',NoSQLprovider).set_logger(None).set_default_compartment(os.getenv('NOSQL_COMP_ID'))
 handle = NoSQLHandle(NoSQLconfig)
