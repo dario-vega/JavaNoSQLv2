bba303361ccaf884b44ca6398c26fc05720c5240
Javier Dario VEGA BAEZ <66302636+dario-vega@users.noreply.github.com>
2021-09-06 19:58:39 +0200

    Create dario2.py

---
+++ dario2.py
@@ -0,0 +1,23 @@
+import os
+import oci
+
+import borneo
+import sys
+from borneo import (
+    AuthorizationProvider, DeleteRequest, GetRequest,
+    IllegalArgumentException, NoSQLHandle, NoSQLHandleConfig, PutRequest,
+    QueryRequest, Regions, TableLimits, TableRequest)
+from borneo.iam import SignatureProvider
+
+signer = oci.auth.signers.InstancePrincipalsDelegationTokenSigner (delegation_token=os.getenv('OCI_obo_token'))
+NoSQLprovider = SignatureProvider(provider=signer)
+NoSQLconfig = NoSQLHandleConfig(os.getenv('OCI_REGION'),NoSQLprovider).set_logger(None).set_default_compartment(os.getenv('NOSQL_COMP_ID'))
+#NoSQLconfig = NoSQLHandleConfig('us-ashburn-1',NoSQLprovider).set_logger(None).set_default_compartment(os.getenv('NOSQL_COMP_ID'))
+handle = NoSQLHandle(NoSQLconfig)
+#
+# Get the row
+#
+request = GetRequest().set_key({'ticketNo': '176233524485'}).set_table_name('demo')
+result = handle.get(request)
+print(' get: ' + str(result))
+handle.close();
